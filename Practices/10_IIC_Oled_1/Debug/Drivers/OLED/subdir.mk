7935e95ce3f60301ad702c560ebe876cebc57b74
rtlab <rtlab1417@gmail.com>
2025-06-26 16:04:11 +0800

    update

---
+++ Practices/10_IIC_Oled_1/Debug/Drivers/OLED/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/OLED/ssd1306.c \
+../Drivers/OLED/ssd1306_fonts.c 
+
+OBJS += \
+./Drivers/OLED/ssd1306.o \
+./Drivers/OLED/ssd1306_fonts.o 
+
+C_DEPS += \
+./Drivers/OLED/ssd1306.d \
+./Drivers/OLED/ssd1306_fonts.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/OLED/%.o Drivers/OLED/%.su Drivers/OLED/%.cyclo: ../Drivers/OLED/%.c Drivers/OLED/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I"D:/STM32CubeIDE/workspace/10_IIC_Oled_1/Drivers/OLED" -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Drivers-2f-OLED
+
+clean-Drivers-2f-OLED:
+	-$(RM) ./Drivers/OLED/ssd1306.cyclo ./Drivers/OLED/ssd1306.d ./Drivers/OLED/ssd1306.o ./Drivers/OLED/ssd1306.su ./Drivers/OLED/ssd1306_fonts.cyclo ./Drivers/OLED/ssd1306_fonts.d ./Drivers/OLED/ssd1306_fonts.o ./Drivers/OLED/ssd1306_fonts.su
+
+.PHONY: clean-Drivers-2f-OLED
+
